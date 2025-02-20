ae46eea84bc788d025f0d204813f7dde556dae4b
eniac11 <hadleym200@gmail.com>
2025-02-20 10:06:21 +0200

    ViewModel for Movie Genre
    A ViewModel is render layer concept that enable the type safe access of
    a Grouping of fields that may not come from the same source.
    
    It is like type safe ViewData or ViewBag.

---
+++ MvcMovie/ViewModels/MovieGenreViewModel.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MvcMovie.Models;
+
+namespace MvcMovie.ViewModels;
+
+public class MovieGenreViewModel {
+    public List<Movie>? Movies {get; set;}
+    public SelectList? Genres {get; set;}
+    public string? MovieGenre {get; set;}
+    public string? SearchString {get; set;}
+}
